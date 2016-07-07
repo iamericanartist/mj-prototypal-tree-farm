110ce47668be93c8fb5646eee5436321f0c4f1e9
Eric Denton <eric@iamericanartist.com>
2016-07-06 23:56:57 -0500

    made good progress on the functionality, next to work on automation and testing

--- spec/HelloWorldSpec.js
+++ spec/HelloWorldSpec.js
@@ -1,44 +1,40 @@
-///////////////////// EXAMPLE ONLY /////////////////////
-describe("Hello world", function() {
-  it("says hello", function() {
-    expect(helloWorld()).toEqual("Hello world!");
-  });
-});
+///////////////////// Unit Tests /////////////////////
+describe("The specifications for growing plants and trees", function(){
 
-//Begin unit testing here with opening "describe() function"
-describe("The specification for basic math", function() {
-
-  // Write a unit test to check for the existence of add()
-  it("should have an add function", function() {
-    expect(add).toBeDefined();
-  });
-  // Write a unit test that will verify the expected output of add()
-  it("should add two integers", function() {
-    expect(add(2, 5)).toBe(7);
-  });
-});
-
-
-///////////////////// Begin Unit Tests /////////////////////
 // 1. There should be a `Plant` function defined.
-
+  it("should have a `Plant` function", function(){
+    expect(Plant).toBeDefined();
+  })
 
 // 2. There should be a `Tree` function defined.
-
+  it("should have a 'Tree' function", function(){
+    expect(Tree).toBeDefined();
+  })
 
 // 3. There should be a function named `increaseHeight` on the prototype of Plant.
-
+  it("should have a function named 'increaseHeight' on the Plant prototype", function(){
+    expect(Plant.prototype.increaseHeight).toBeDefined();
+  })
 
 // 4. There should be a function named `decreaseHeight` on the prototype of Plant.
-
+  it("should have a function named 'decreaseHeight' on the Plant prototype", function(){
+    expect(Plant.prototype.decreaseHeight).toBeDefined();
+  })
 
 // 5. There should be a `height` property on Plant.
-
+  it("should have a 'height' property on Plant", function(){
+    expect(Plant.height).toBeDefined();
+  })
 
 // 6. There should be a function named `grow` on the prototype of Tree.
-
+  it("should have a funcion named 'grow' on the prototype of Tree", function(){
+    expect(Tree.prototype.grow).toBeDefined();
+  })
 
 // 7. There should be a function named `trim` on the prototype of Tree.
+  it("should have a funcion named 'trim' on the prototype of Tree", function(){
+    expect(Tree.prototype.trim).toBeDefined();
+  })
 
 
 // 8. When you invoke the `trim` method on Tree, the height of the Tree should be reduced by the integer value that is passed as an argument (e.g. `PearTree.trim(2)`)
@@ -50,3 +46,4 @@ describe("The specification for basic math", function() {
 // 10. When you invoke the `grow` method on Tree, the height of the Tree should be increased by the integer value that is passed as an argument (e.g. `PearTree.grow(2)`)
 
 
+}); //closing of our ORCHARD DESCRIPTION function
